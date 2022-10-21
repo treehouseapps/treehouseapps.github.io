3c6ffa33db6f4a4fc4a4fbf0705127fc5ddc0d9c
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2022-10-21 18:52:35 +0300

    new update

---
+++ img/Javascript.js
@@ -0,0 +1,54 @@
+alert("WELLCOME TO TREE HOUSE SHOP Center\n\n YOU ARE AWARDED 1000 $ FOR BEING HERE");
+var a = prompt("ENTER YOUR NAME")
+function aa(){
+var x = document.getElementById("1");
+var y = document.getElementById("2");
+var zz = x.value - y.value;
+x.value = zz;
+if(x.value == 0){
+alert("DEAR "+a+"\n NOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ab(){var x = document.getElementById("1");
+var y = document.getElementById("3");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("YOUR BALANCE IS INSUFFICIENT !!!\n\n PRESS OK FOR CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ac(){var x = document.getElementById("1");
+var y = document.getElementById("4");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ad(){var x = document.getElementById("1");
+var y = document.getElementById("5");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ae(){var x = document.getElementById("1");
+var y = document.getElementById("6");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function af(){var x = document.getElementById("1");
+var y = document.getElementById("7");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ag(){var x = document.getElementById("1");
+var y = document.getElementById("8");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+function ah(){var x = document.getElementById("1");
+var y = document.getElementById("9");
+var zz = x.value - y.value;
+x.value = zz;if(x.value == 0){
+alert("DEAR "+a+"\nNOW YOU ARE IN CREDIT SERVICE ");}
+else if(x.value < 0){alert("Credit Added")}}
+if(x.value == 0){
+alert("a");}
