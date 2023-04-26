d9151fb686a756ac3a4dc4fb408a66a2239629e8
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2023-04-26 23:02:02 +0300

    Update Script.js

--- counter/Script.js
+++ counter/Script.js
@@ -1,31 +1,33 @@
+var count = document.getElementById("no");
+var payment = document.getElementById("bi");
+var warn = document.getElementById("ow");
 
-  var count = document.getElementById("no");
-  var payment = document.getElementById("bi");
-  var warn = document.getElementById("ow");
-  
-  var bill = 0;
-  function s(){
-    setInterval(function(){
-    do{
-  count.value++;}
-  while (count.value > 6)
-  if(count.value == 6){
-    count.value = 0;
-    bill = bill + 10; 
-    payment.value = bill;
-  }
-  if (bill > 10){
-    payment.style.color = "red";
-    warn.innerHTML = "Dude it is increasing just pay !";
-  }
-  else if (bill < 10){
-    payment.style.color = "black";
-  }
+var bill = 0;
+function s(){
+  const hi = setInterval(function()
+  { count.value++;
+    if(count.value == 6){
+        alert("$10 added to your bill")
+        bill = bill + 10; 
+        payment.value = bill;
+        count.value = 0;
+        clearInterval(hi);
+    
+        if(bill > 10){
+          payment.style.color = "red";
+          warn.innerHTML = "Dude it is increasing just pay !";
+        }  
+    }  
+  }, 1000);
 
-}, 1000);
-  }
-  function r(){
-    payment.value = 0;
-    bill = 0;
-    warn.innerHTML = "";
-  }
+}
+function r(){
+  if(payment.value > 5){
+  payment.value = 0;
+  bill = 0;
+  warn.innerHTML = "";
+  alert("finally ... Bill Payed");
+  payment.style.color = "black";
+}
+else{ alert("Relax man ... there is no bill to pay"); }
+}
