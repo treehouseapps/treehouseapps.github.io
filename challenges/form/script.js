821fe5f790548e2d009b62fb17fb9e495abce363
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2024-09-24 19:48:41 +0300

    update

--- challenges/form/script.js
+++ challenges/form/script.js
@@ -11,48 +11,125 @@ myForm.addEventListener("submit", (event) => {
 function validateForm() {
     let isValid = true;
     const nameInput = document.getElementById("name");
+    const dateInput = document.getElementById("date");
+    const selectedgender = document.querySelector('input[name="gender"]:checked');
+    const fileInput = document.getElementById("file");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const pal = document.getElementById("pal");
+    const cpasswordInput = document.getElementById("cPassword");
     nameInput.style.borderColor = "black"
 
+    //Name
     if (nameInput.value.length < 5) {
-        alert("Name is too short.");
+        ual.style.display = "block";
+        ual.innerText = "Name is too short.";
         nameInput.style.borderColor = "red"
         isValid = false;
     }
+    else {
+        ual.style.display = "none";
+        nameInput.style.borderColor = "black"
+    }
 
-    const emailInput = document.getElementById("email");
+    //Email
     if (!validateEmail(emailInput.value)) {
-        alert("Please enter a valid email address.");
+        eal.style.display = "block";
+        eal.innerText = "Please enter a valid email address.";
         emailInput.style.borderColor = "red"
         isValid = false;
     }
-    const passwordInput = document.getElementById("password");
-    const cpasswordInput = document.getElementById("cPassword");
-    if (cpasswordInput.value != passwordInput.value) {
-        cpasswordInput.style.borderColor = "red"
+    else {
+        eal.style.display = "none";
+        emailInput.style.borderColor = "black"
+    }
+
+    //Password
+    if (passwordInput.value.length < 2) {
+        pal.style.display = "block";
+        pal.innerHTML = "password must be 8 Characters"
+        passwordInput.style.borderColor = "red"
         isValid = false;
     }
-    const dateInput = document.getElementById("date");
+    else if (passwordInput.value.search(/[0-9]/) == -1) {
+        pal.style.display = "block";
+        pal.innerText = "password must include atleast 1 digit"
+        passwordInput.style.borderColor = "red"
+        isValid = false;
+    }
+    else if (passwordInput.value.search(/[a-z]/) == -1) {
+        pal.style.display = "block";
+        pal.innerText = "password must inclide atleast 1 lowercase"
+        passwordInput.style.borderColor = "red"
+        isValid = false;
+    }
+    else if (passwordInput.value.search(/[A-Z]/) == -1) {
+        pal.style.display = "block";
+        pal.innerText = "password must include atleast 1 uppercase"
+        passwordInput.style.borderColor = "red"
+        isValid = false;
+    }
+    else if (passwordInput.value.search(/[!\!\@\#\$\%\^\&\*\(\)\-\_\+\=]/) == -1) {
+        pal.style.display = "block";
+        pal.innerText = "password must include atleast 1 Special Character"
+        passwordInput.style.borderColor = "red"
+        isValid = false;
+    }
+    else {
+        pal.style.display = "none";
+        passwordInput.style.borderColor = "black"
+    }
+
+    //Confirm Password
+    if (cpasswordInput.value !== passwordInput.value) {
+        cpasswordInput.style.borderColor = "red"
+        cpal.style.display = "block";
+        cpal.innerText = "Password does not match"
+        isValid = false;
+    } else {
+
+        cpal.style.display = "none";
+        cpasswordInput.style.borderColor = "black"
+    }
+
+    //Date
     const selectedDate = new Date(dateInput.value);
     const today = new Date();
     if (selectedDate >= today) {
-        alert("Date of birth must be a date in the past.");
+        dal.style.display = "block";
+        dal.innerText = "Date of birth must be a date in the past.";
+        dateInput.style.borderColor = "red"
         isValid = false;
     }
-    const fileInput = document.getElementById("file");
+    else {
+        dal.style.display = "none";
+        dateInput.style.borderColor = "black"
+    }
+
+    //File
     const file = fileInput.files[0];
 
     if (file) {
         const fileExtension = file.name.split('.').pop().toLowerCase();
         if (fileExtension !== 'png' && fileExtension !== 'jpg') {
-            alert("Please upload a file in PNG or JPG format.");
+            fal.style.display = "block";
+            fal.innerText = "Please upload a file in PNG or JPG format.";
             isValid = false;
+        }
+        else {
+            fal.style.display = "none";
+            fileInput.style.borderColor = "black"
         }
     }
 
-    const selectedgender = document.querySelector('input[name="gender"]:checked');
+    //Gender
     if (!selectedgender) {
-        alert("Please select gender ");
+        gal.style.display = "block";
+        gal.innerText = "Please select gender ";
         isValid = false;
+    }
+    else {
+        gal.style.display = "none";
     }
     return isValid;
 }
@@ -60,4 +137,5 @@ function validateForm() {
 function validateEmail(email) {
     const re = /^[^s@]+@[^s@]+.[^s@]+$/;
     return re.test(email);
+
 }
